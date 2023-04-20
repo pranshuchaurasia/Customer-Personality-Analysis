c8dfc96e15b7917e8755214a9716d63502835bd6
Pranshu <pranshuorian001@gmail.com>
2023-04-20 17:22:44 +0530

    adding the customer marketing data to mongo db

---
+++ data_dump.py
@@ -0,0 +1,21 @@
+import pymongo
+import pandas as pd
+import json
+
+# Provide the mongodb localhost url to connect python to mongodb.
+client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+
+DATA_FILE_PATH="/config/workspace/marketing_campaign.csv"
+DATABASE_NAME="customer_data"
+COLLECTION_NAME="marketing_campaign"
+
+if __name__=="__main__":
+    df=pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and Columns: {df.shape}")
+    #Convert dataframe to JSON so that we can dump the record in mongodb
+    df.reset_index(drop=True, inplace=True)
+    json_record=list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+
+    #inser json record into mongo db
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
