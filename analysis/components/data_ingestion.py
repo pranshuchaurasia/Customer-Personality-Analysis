511c17934e9e961287e486e3c562de360fea2549
Pranshu <pranshuorian001@gmail.com>
2023-05-28 23:31:50 +0530

    Logging the data

--- analysis/components/data_ingestion.py
+++ analysis/components/data_ingestion.py
@@ -12,25 +12,39 @@ from sklearn.model_selection import train_test_split
 class DataIngestion:
     def __init__(self,data_ingestion_config:config_entity.DataIngestionConfig):
         try:
-            
+            logging.info(f"{'>>'*20} Data Ingestion {'<<'*20}")
             self.data_ingestion_config = data_ingestion_config
         except Exception as e:
             raise AnalysisException(e,sys)
 
     def initiate_data_ingestion(self)->artifact_entity.DataIngestionArtifact:
         try:
+            logging.info(f"Exporting collection data as pandas dataframe")
             df:pd.DataFrame =utils.get_collection_as_dataframe(
                 database_name=self.data_ingestion_config.database_name,
                 collection_name=self.data_ingestion_config.collection_name
             )
             df.replace(to_replace="na",value=np.NAN,inplace=True)
 
-            
+            logging.info("Save data in feature store")
+
+            #Save data in feature store
+            logging.info("Create feature store folder if not available")
+
+            logging.info("create dataset directory folder if not available")
+            #create dataset directory folder if not available
             feature_store_dir=os.path.dirname(self.data_ingestion_config.feature_store_file_path)
+
+            logging.info("Save df to feature store folder")
+            #Save df to feature store folde
             os.makedirs(feature_store_dir,exist_ok=True)
 
+
+            
             df.to_csv(path_or_buf=self.data_ingestion_config.feature_store_file_path,index=False,header=True)
 
+            logging.info("split dataset into train and test set")
+            #split dataset into train and test set
             train_df,test_df = train_test_split(df,test_size=self.data_ingestion_config.test_size)
 
 
@@ -41,7 +55,8 @@ class DataIngestion:
             train_df.to_csv(path_or_buf=self.data_ingestion_config.train_file_path,index=False,header=True)
             test_df.to_csv(path_or_buf=self.data_ingestion_config.test_file_path,index=False,header=True)
             
-        
+            #Prepare artifact
+            logging.info(f"Data ingestion artifact: {data_ingestion_artifact}")
 
             data_ingestion_artifact = artifact_entity.DataIngestionArtifact(
                 feature_store_file_path=self.data_ingestion_config.feature_store_file_path,
